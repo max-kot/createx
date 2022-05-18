b7efcc2629f5d4ea2677c5cebf93a927bcd0e20a
sim-max <101747552+sim-max@users.noreply.github.com>
2022-05-18 23:03:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const menuButton = document.querySelector('#menu-btn');
 const menuList = document.querySelector('#menu-list')
 const menuElements = document.querySelectorAll('.header__menu-list li')
 menuButton.addEventListener('click', function () {
-	if (menuList.classList.toggle('active') && menuButton.classList.toggle('active-btn')) {
+	if (menuList.classList.toggle('active')) {
+		menuButton.classList.toggle('active-btn');
 		menuElements.forEach(function (item) {
 			item.addEventListener('click', function () {
 				menuList.classList.remove('active')
